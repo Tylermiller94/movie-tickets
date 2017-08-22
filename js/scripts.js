7b23475319f1a07cc6b8bb2d58083b9c1978bb0d
Kristen Kulha and Tyler Miller <tyler@>
2017-08-22 13:32:06 -0700

    add function to calculate cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
 //Business Logic
 
 function Ticket(movie, time, age){
-  var price = movie + time + age;
-  return price;
-
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
 }
 
-
-
+function cost(ticket) {
+  var cost = 0;
+  if(ticket.movie.includes("Hoarders") || ticket.movie.includes("Fast") || ticket.movie.includes("3D")) {
+    cost += 15;
+  } else {
+    cost += 10;
+  }
+  if(ticket.time.includes("PM")){
+    cost +=2;
+  }else {
+    cost -=1;
+  }
+  if(ticket.age.includes("Senior")){
+    cost -=2;
+  }
+  return cost;
+}
 
 
 //Interface Logic
@@ -15,8 +30,17 @@ $(document).ready(function() {
   $("#movies-form").submit(function(event){
     event.preventDefault();
 
-    var movie = parseInt($("#movies").val());
-    var time =  parseInt($("#times").val());
-    var age =   parseInt($("#age").val());
+    var movie = $("#movies").val();
+    var time =  $("#times").val();
+    var age =   $("#age").val();
+    var ticket =  new Ticket(movie, time, age);
+    var price = cost(ticket);
+    console.log(ticket);
+    console.log(cost(ticket));
+     $("#price").show();
+
+     $("#movies-form").hide();
+     $("#cost").append("$" + price);
+     $("#price").append("Your ticket is for " + movie + " at " + time);
   });
 });
